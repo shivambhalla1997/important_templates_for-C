e741777c148b7a5da73787272e71e34f1be96ef3
shivambhalla1997 <31740957+shivambhalla1997@users.noreply.github.com>
2019-01-16 23:47:39 +0530

    add function to phone-book

--- multipe_input.cpp
+++ multipe_input.cpp
@@ -5,11 +5,16 @@
 #include<sstream>
 #include<string>
 #include<iostream>
+#include<vector>
+#define mod 1000000007
+#define hash_size 100000
 
 using namespace std;
 
 int main()
 {
+    vector<pair<string,long long int>>hash_table[hash_size]; // creating hash table
+    memset(hash_table,0,sizeof(hash_table));
     long long int N,phone_number;
     char name[16],action[5];
     cin>>N;
@@ -20,10 +25,23 @@ int main()
         getline(cin,str);
         if(i!=0)
         {
-            stringstream ss(str);
-        ss>>action>>phone_number>>name;
-        cout<<action<<endl;
+            if(str[0]=='a') // when we are adding the number 3 input
+            {
+                stringstream ss(str);
+                ss>>action>>phone_number>>name;
+            }
+            else  // when deleting and finding only 2 inputs
+            {
+                stringstream ss(str);
+                ss>>action>>phone_number;
+            }
             
+            
+            if(action[0]=='a') // during add command
+            {
+                long long int key=(((phone_number)%mod)%hash_size);
+                hash_table[key].push_back(make_pair(name,phone_number));
+            }
         }
     }
     return 0;
